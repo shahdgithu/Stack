1cb8eb2e97239b885ecac7684cfacfb69a20c02c
shahdgithu <111149310+shahdgithu@users.noreply.github.com>
2023-03-07 21:40:59 +0300

    Add files via upload

---
+++ LinkedStack.java
@@ -0,0 +1,51 @@
+
+public class LinkedStack<E>implements Stack<E> {
+
+    SinglyLinkedList<E> list = new SinglyLinkedList<>();
+
+
+    @Override
+    public boolean isEmpty() {
+        return list.isEmpty();
+    }
+
+    @Override
+    public int size() {
+        return list.size();
+    }
+
+    @Override
+    public E top() {
+        return list.first();
+    }
+
+    @Override
+    public void push(E e) {
+        list.addFirst(e);
+    }
+
+    @Override
+    public E pop() {
+        return list.removeFirst();
+    }
+
+
+
+    public void transfer(Stack<E> S,Stack<E> T)
+    {
+        while (!S.isEmpty())
+        {
+            T.push(S.pop());
+        }
+    }
+    public void removeAll()
+    {
+        while (!isEmpty())
+        {
+            pop();
+        }
+    }
+
+
+
+}
